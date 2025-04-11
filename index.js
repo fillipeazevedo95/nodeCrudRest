17ead697b37a552d596dce9171f6f5440b51a643
Fillipe Azevedo <fillipe.azevedo@intra.grupoginseng.com.br>
2025-04-11 10:18:57 -0300

    criando o post

--- index.js
+++ index.js
@@ -13,12 +13,22 @@ server.get("/customers", (req, res) => {
   return res.json(customers);
 });
 
-server.get("customers/:id", (req, res) => {
+server.get("/customers/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const customer = customers.find((item) => item.id === id);
   const status = customer ? 200 : 404;
 
   return res.status(status).json(customer);
+});
+
+server.post("/customers", (req, res) => {
+  const { name, site } = req.body;
+  const id = customers[customers.length - 1].id + 1;
+
+  const newCustomer = { id, name, site };
+  customers.push(newCustomer);
+
+  return res.status(201).json(newCustomer);
 });
 
 server.listen(3000);

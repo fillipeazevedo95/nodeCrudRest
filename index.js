fa9ce0147cf693f9bbb7e80b562776565ea098e7
Fillipe Azevedo <fillipe.azevedo@intra.grupoginseng.com.br>
2025-04-11 10:08:29 -0300

    criando rotas get

--- index.js
+++ index.js
@@ -9,4 +9,16 @@ let customers = [
   { id: 3, name: "Wilson", site: "http://wilson.com.br" },
 ];
 
+server.get("/customers", (req, res) => {
+  return res.json(customers);
+});
+
+server.get("customers/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const customer = customers.find((item) => item.id === id);
+  const status = customer ? 200 : 404;
+
+  return res.status(status).json(customer);
+});
+
 server.listen(3000);
